71a9852ded112fef58618d41dab281fb63a2ed1b
Marko4123 <marko.uzunov@gmail.com>
2016-03-29 18:50:53 +0300

    Create Задача11.java

---
+++ chapter3/Задача11.java
@@ -0,0 +1,28 @@
+/*Дадено e число n и позция p. Напишете поредица от опреции, които да отпечатват стойността на бита на позиция
+ * p от числото n (0 или 1).
+ * 
+ * 
+ * */
+
+import java.util.Scanner;
+public class GetPositionBit {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		System.out.print("Въведете число \"n\" : ");
+		int n = input.nextInt();
+		
+		System.out.print("Въведете брой позции \"p\" : ");
+		int p = input.nextInt();
+		
+		//Създаване на маска за манипулиране на битовете на числото 1
+		int mask = 1 << p; //Двоичен вид на числото 1, изместено с зададен брой позции
+		
+		int result = n & mask;
+		result = result >> p;
+		
+		System.out.println(result);
+	}
+
+}
