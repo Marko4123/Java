7aebe4ce94dbe7df6b3b4d9806e226f2a2376839
Marko4123 <marko.uzunov@gmail.com>
2016-04-03 17:23:22 +0300

    Create ifelseConstruction.java

---
+++ chapter5/ifelseConstruction.java
@@ -0,0 +1,20 @@
+package ifelseconstruction;
+
+/**
+ *
+ * @author Marko
+ */
+public class IfelseConstruction {
+
+   
+    public static void main(String[] args) {
+        int x = 3;
+        if (x > 3) {
+            System.out.println("x е по-голямо от 3");
+        }
+        else {
+            System.out.println("x не е по-голямо от 3");
+        }
+    }
+
+}
