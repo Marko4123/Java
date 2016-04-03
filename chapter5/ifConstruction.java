a41eaa066febb2282a697812ad7c0f5e494ffa4b
Marko4123 <marko.uzunov@gmail.com>
2016-04-03 17:17:54 +0300

    Create ifConstruction.java

---
+++ chapter5/ifConstruction.java
@@ -0,0 +1,18 @@
+package ifconstructionv2;
+
+/**
+ *
+ * @author Marko
+ */
+public class IfConstructionV2 {
+
+   
+    public static void main(String[] args) {
+        int a = 6;
+        
+        if (a > 5) {
+            System.out.println("Променливата е по-голяма от 5.");
+        }
+    }
+
+}
