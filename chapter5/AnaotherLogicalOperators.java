0bb40b8eae10335aa9bed3a6e79834cfcc3bb344
Marko4123 <marko.uzunov@gmail.com>
2016-04-03 16:17:51 +0300

    Create AnaotherLogicalOperators.java

---
+++ chapter5/AnaotherLogicalOperators.java
@@ -0,0 +1,18 @@
+
+
+package anaotherlogicaloperators;
+
+/**
+ *
+ * @author Marko
+ */
+public class AnaotherLogicalOperators {
+
+   
+    public static void main(String[] args) {
+        System.out.println((2<3) || (3<2)); // true
+        System.out.println((2<3) ^ (4>3)); // false
+        System.out.println (!(7 == 5)); // true
+    }
+
+}
