b900e7e4fa4ef941130f2afee516bf24233c5b56
Marko4123 <marko.uzunov@gmail.com>
2016-04-03 15:42:27 +0300

    Create CompareFloatingPointTypesV1.java

---
+++ chapter5/CompareFloatingPointTypesV1.java
@@ -0,0 +1,21 @@
+
+
+package comparefloatingpointtypes;
+
+/**
+ *
+ * @author Marko
+ */
+public class CompareFloatingPointTypes {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float value = 1.0f;
+        System.out.println(value);
+        float sum = 0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f+0.1f;
+        System.out.println(sum);
+        System.out.println(sum == value); // false
+    }
+
+}
