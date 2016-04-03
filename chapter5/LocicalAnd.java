c29774a1ea73bb2f50c6a768c38d7e553bf163e8
Marko4123 <marko.uzunov@gmail.com>
2016-04-03 16:07:56 +0300

    Create LocicalAnd.java

---
+++ chapter5/LocicalAnd.java
@@ -0,0 +1,17 @@
+
+
+package logicaland;
+
+/**
+ *
+ * @author Marko
+ */
+public class LogicalAnd {
+
+   
+    public static void main(String[] args) {
+        boolean b = true && false;
+        System.out.println("b = " + b);
+    }
+
+}
